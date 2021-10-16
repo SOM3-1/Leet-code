5e3fb439eb5edc15f06f9158d96a4a8c3642277c
Dushyanth N Gowda <dushgowda@gmail.com>
2021-10-16 16:08:52 +0530

    Update bs-algo.js

--- bs-algo.js
+++ bs-algo.js
@@ -6,26 +6,20 @@
 
 
 //iterative   
-const search = function(nums, target) {
-    let low = 0;
-    let high = nums.length-1;
-  console.log(`length = ${high}`);
-  
-    while (low<=high)
-        {
-            let ind = low + (high-low)/2;
-            if(nums[ind]===target)
-                return ind;
-            if( target>nums[ind])
-                high = ind+1;
-            else
-                low = ind-1;
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        if(nums[mid] === target) {
+            return mid
+        } else if(nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
         }
-    return -1;
-    
+    }
+    return -1
 };
-
-const z =search([-1,0,3,5,9,12] , 9);
 
 //leet code
 /**
