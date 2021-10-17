07bb933bfbb79a79dce0ed9825c2f456e9b86586
Dushyanth N Gowda <dushgowda@gmail.com>
2021-10-17 18:01:55 +0530

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -3,3 +3,8 @@ const rotate = (nums, k)=> {
     return [...nums.splice(k+1),...nums];
 };
 
+//online solution
+const rotate = (nums, k) => {
+    nums.unshift(...nums.splice(nums.length-k,k))
+};
+
